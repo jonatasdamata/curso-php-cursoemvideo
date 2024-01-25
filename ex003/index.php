b1cb263b6d7f2be393eb40e000c2285fb38df757
Jonatas da  Mata <jonatasdamata1@hotmail.com>
2024-01-25 17:32:12 -0300

    wip: adicionando códigos

---
+++ ex003/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // $num = 0x1A;
+        // echo "O valor da variável é $num";
+
+        // $v = "Jonatas";
+        // var_dump($v);
+
+        // $num = 3e2;
+        // echo "O valor é $num";
+
+        $num = "950";
+        var_dump($num)
+
+    ?>
+</body>
+</html>
