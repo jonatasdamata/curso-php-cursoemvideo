2e059ef4a5f48e43b36b5e12ad0995fe469f8322
Jonatas da  Mata <jonatasdamata1@hotmail.com>
2024-01-31 09:44:08 -0300

    wip

---
+++ desafio03/conversao.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 03 PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Conversor de Moedas</h1>
+        <?php 
+            $cotação = 5.17;
+
+            $real = $_REQUEST["din"] ?? 00;
+
+            $dólar = $real / $cotação;
+
+            $padrão = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+
+            echo "Seus " . numfmt_format_currency($padrão, $real, "BRL") . " equivalem a " . numfmt_format_currency($padrão, $dólar, "USD" ) . "</p>";
+        ?>
+        <button onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+</body>
+</html>
